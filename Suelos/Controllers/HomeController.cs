88358f667de2809468d4249af4aed823653c59d2
lababativa <36833521+lababativa@users.noreply.github.com>
2020-10-24 10:49:17 -0500

    Agregar archivos de proyecto.

---
+++ Suelos/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Suelos.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sing_up()
+        {
+            ViewBag.Message = "Registro";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Ingreso";
+
+            return View();
+        }
+    }
+}
